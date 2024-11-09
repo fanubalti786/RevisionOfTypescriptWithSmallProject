8c8bbb5387b21e234f6c70089073ba274bd138d1
fanubalti786 <fanubalti786@gmail.com>
2024-11-09 17:26:43 +0500

    update

---
+++ lecture1(VariableInTypscript)/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var userName = "irfan";
+var num1 = 22;
+var num2 = 23;
+var result = num1 + num2;
+console.log(userName, num1, num2, result);
---
+++ lecture1(VariableInTypscript)/index.ts
@@ -0,0 +1,6 @@
+var userName:string = "irfan";
+var num1:number = 22;
+var num2:number = 23;
+var result:number = num1 + num2;
+
+console.log(userName,num1,num2,result);
